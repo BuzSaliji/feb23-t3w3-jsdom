49ed7b8a8c92432921431f2cdee9c6a132238343
BuzSaliji <buz.saliji@gmail.com>
2023-08-31 20:34:22 +1000

    feat: pseudo form text hint

--- script.js
+++ script.js
@@ -54,7 +54,13 @@ function removeItemFromList(targetItem) {
     //targetItemNode.parentNode.removeChild(targetItemNode);
 
     // Modify the array
-    favouriteMedia = favouriteMedia.filter((item) => item !== targetItem);
+    favouriteMedia = favouriteMedia.filter((item) => {
+      if (!item || item == "" || item !== targetItem) {
+        return true;
+      } else {
+        return false;
+      }
+    });
 
     // Regenerate the visuals
     createListOfMedia();
@@ -67,15 +73,52 @@ function addItemToList(event) {
 
   let realInputField = document.getElementById("real-nameinput");
   let newItemName = realInputField.value;
-  console.log("newItemName is: " + newItemName);
-  // add item to list
-  favouriteMedia.push(newItemName);
+  if (newItemName) {
+    console.log("newItemName is: " + newItemName);
+    // add item to list
+    favouriteMedia.push(newItemName);
 
-  // generate a new list
-  createListOfMedia();
+    // generate a new list
+    createListOfMedia();
+  } else {
+    console.warn("Attempted to add an empty item to the list");
+    console.error("Example error");
+  }
 }
 
 let realFormSubmitButton = document.getElementById("real-formsubmit");
 realFormSubmitButton.addEventListener("click", addItemToList);
 // functionName() runs immediately, do not want!!
 // realFormSubmitButton.addEventListener("click", addItemToList());
+
+// helper text id: real-texthint
+
+function inputHelperOnFocus(targetId) {
+  let helperElement = document.getElementById("real-texthint");
+  console.log("Showing text hint now");
+  helperElement.style.display = "inherit";
+}
+
+function inputHelperOnBlur(targetId) {
+  let helperElement = document.getElementById("real-texthint");
+  console.log("Showing text hint now");
+  helperElement.style.display = "none";
+}
+
+let realFormTextInput = document.getElementById("real-nameinput");
+realFormTextInput.addEventListener("focusin", () => {
+  inputHelperOnFocus("real-texthint");
+});
+realFormTextInput.addEventListener("focusout", () => {
+  inputHelperOnBlur("real-texthint");
+});
+inputHelperOnBlur("real-texthint");
+
+let pseudoFormTextInput = document.getElementById("pseudo-nameinput");
+pseudoFormTextInput.addEventListener("focusin", () => {
+  inputHelperOnFocus("pseudo-texthint");
+});
+pseudoFormTextInput.addEventListener("focusout", () => {
+  inputHelperOnBlur("pseudo-texthint");
+});
+inputHelperOnBlur("pseudo-texthint");
